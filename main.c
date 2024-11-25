210bfc8a7ce93a09b1b7147d4580dd4206f5df62
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2024-11-25 22:19:52 +0700

    Bài 3

--- main.c
+++ main.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
-int main() {
-    int target = 62;
-    int user_input;
-    printf("Nhap so trung voi %d de ket thuc bai.\n", target);
-
-    do {
-        printf("Hay nhap so: ");
-        scanf("%d", &user_input);
-        
-        if (user_input != target) {
-            printf("Ban nhap chua dung , hay thu lai.\n");
-        }
-    } while (user_input != target);
-
- 
-    printf("Chuc mung! Ban da nhap dung so %d.\n", target);
-
+#include<stdio.h>
+int main(){
+    int i;
+    int n;
+    int sum=0;
+    printf("Ban hay nhap so nguyen duong ");
+    scanf("%d", &n);
+    if (n <=0) {
+        printf("Ban nhap sai roi");
+    }
+    for (i = 0; i <= n; i++){
+        sum += i;
+    }
+    printf("Tong %d la %d", n,sum);
     return 0;
 }
+
+
 
