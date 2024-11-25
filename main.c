de5cf95866a3ec080a6e09b9bff5faf5c7f2bef6
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2024-11-25 21:42:07 +0700

    Bài tập về nhà Seson.2

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
-
 #include <stdio.h>
-
 int main() {
-    for (int i = 100; i >= 1; i--){
-        printf("%d\n", i);
-    }
+    int target = 62;
+    int user_input;
+    printf("Nhap so trung voi %d de ket thuc bai.\n", target);
+
+    do {
+        printf("Hay nhap so: ");
+        scanf("%d", &user_input);
+        
+        if (user_input != target) {
+            printf("Ban nhap chua dung , hay thu lai.\n");
+        }
+    } while (user_input != target);
+
+ 
+    printf("Chuc mung! Ban da nhap dung so %d.\n", target);
+
     return 0;
 }
+
